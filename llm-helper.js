00b192fcd205f2fc8a378d27a3d3e1c7a49015fe
dchang <dchang@uncharted.software>
2023-06-28 07:43:25 -0400

    init commit, some examples and tests

---
+++ llm-helper.js
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from 'openai';
+import env from './env.json' assert { type: 'json' };
+
+const configuration = new Configuration({
+  apiKey: env.OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Basic pass through
+export const questionPrompt = async (text) => {
+  const res = await openai.createCompletion({
+    model: 'text-davinci-003',
+    max_tokens: 2000,
+    prompt: text
+  });
+  return res.data.choices[0].text; 
+}
+
+
+// Summarize conversation
+export const summaryPrompt = async (text) => {
+  const res = await openai.createCompletion({
+    model: 'text-davinci-003',
+    max_tokens: 2500,
+    temperature: 0.02,
+    prompt: `
+	The following is a conversation: 
+
+	${text}
+
+	Summarize the conversation in a few short sentences.
+	`
+  });
+  return res.data.choices[0].text; 
+}
+
+
+// Synthesize new knowledge
---
+++ slackbot.js
@@ -0,0 +1,92 @@
+import pkg from '@slack/bolt';
+const { App } = pkg;
+
+import env from './env.json' assert { type: 'json' };
+import { questionPrompt, summaryPrompt } from './llm-helper.js';
+
+const app = new App({
+  token: env.OAUTH_TOKEN,
+  signingSecret: env.SIGNING_SECRET,
+  appToken: env.APP_TOKEN,
+  socketMode: true // enable to use socket mode
+});
+
+let botUserId = '';
+
+const handleQuestion = async (text, say) => {
+  const result = await questionPrompt(text);
+  return say(result);
+};
+
+const handleSummary = async (channelId, number, say) => {
+  const result = await app.client.conversations.history({
+    channel: channelId
+  });
+
+  const messages = result.messages;
+  const filterdMessages = messages.filter(d => {
+    return !d.subtype && 
+	  !d.bot_profile &&
+	  !d.text.startsWith(`<@${botUserId}>`) &&
+	  d.text.length > 2;
+  });
+
+  const orderedMessages = filterdMessages.reverse().map(d => d.text).splice(number);
+  const text = orderedMessages.join('\n');
+  const llmResult= await summaryPrompt(text); 
+  return say(llmResult); 
+}
+
+const handleHelp = async (say) => {
+  return say(`
+  \`\`\`
+Usage is @bot <command> <context>:
+The commands are:
+Q <question>
+- basically a chatGPT proxy
+
+S [x=50] 
+- summarize roughly the last <x> number of messages
+  \`\`\`
+  `);
+};
+
+
+app.message(/.*/, async ({ message, say }) => {
+  if (message.text.startsWith(`<@${botUserId}>`)) {
+	// console.log(message);
+	const channelId = message.channel;
+
+	// Parse message
+	const [_id, command, ...args] = message.text.split(' ');
+	const userText = args.join(' ');
+	console.log(command, userText);
+
+	if (command === 'Q') await handleQuestion(userText, say);
+	else if (command === 'S') await handleSummary(channelId, +userText, say);
+    else await handleHelp(say);
+  }
+});
+
+
+(async () => {
+  await app.start(3000);
+  console.log('App started'); 
+  const blah = await app.client.auth.test();
+  botUserId = blah.user_id;
+})();
+
+
+/*
+app.message(':wave:', async ({ message, say }) => {
+  console.log('message', message);
+  // Handle only newly posted messages here
+  if (message.subtype === undefined
+    || message.subtype === 'bot_message'
+    || message.subtype === 'file_share'
+    || message.subtype === 'thread_broadcast') {
+    await say(`Hello, <@${message.user}>`);
+  }
+});
+*/
+
